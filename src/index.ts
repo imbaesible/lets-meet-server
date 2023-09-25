be977463933382791aa55b49317e44591b19b3d8
Hemant Ashta <hemant@workongrid.com>
2023-09-25 11:11:26 +0530

    initial setup

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import http from "http";
+
+const port = 8080;
+const app = express()
+const server = http.createServer(app);
+
+server.listen(port, () => {
+    console.log('Gopii ki krda piyan? 💀')
+})
