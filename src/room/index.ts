45461c033dea8adfeda7a8ae7b36d41acb289b5e
Hemant Ashta <hemant@workongrid.com>
2023-11-14 11:18:50 +0530

    added: chat functionality in the backend

--- src/room/index.ts
+++ src/room/index.ts
@@ -2,10 +2,17 @@ import { Socket } from "socket.io";
 import { v4 as uuidV4 } from "uuid";
 
 const rooms: Record<string, string[]> = {}
+const chats: Record<string, IMessage[]> = {}
 
 interface IRoomParams {
     roomId: string;
     peerId: string;
+}
+
+interface IMessage {
+    content: string;
+    author?: string;
+    timestamp: number;
 }
 
 export const roomHandler = (socket: Socket) => {
@@ -16,17 +23,17 @@ export const roomHandler = (socket: Socket) => {
         joinRoom({ roomId, peerId });
     }
     const joinRoom = ({ roomId, peerId }: IRoomParams) => {
-        if(rooms[roomId]){
-            rooms[roomId].push(peerId)
-            socket.join(roomId)
-            socket.to(roomId).emit('user-joined', { roomId, peerId })
-            socket.emit('get-users-list', {
-                roomId,
-                participants: rooms[roomId],
-            })
-        } else {
-            createRoom({ peerId });
-        }
+        if(!rooms[roomId]) rooms[roomId] = [];
+        if(!chats[roomId]) chats[roomId] = [];
+
+        rooms[roomId]?.push(peerId)
+        socket.join(roomId)
+        socket.to(roomId).emit('user-joined', { roomId, peerId })
+        socket.emit('get-messages', chats[roomId])
+        socket.emit('get-users-list', {
+            roomId,
+            participants: rooms[roomId],
+        })
 
         socket.on("disconnect", () => {
             leaveRoom({ roomId, peerId })
@@ -46,8 +53,18 @@ export const roomHandler = (socket: Socket) => {
         socket.to(roomId).emit('user-disconnected', peerId)
     }
 
+    const addMessage = (roomId: string, message: IMessage) => {
+        if(chats[roomId]){
+            chats[roomId].push(message)
+        } else {
+            chats[roomId] = [message]
+        }
+        socket.to(roomId).emit('add-message', message)
+    }
+
     socket.on('create-room', createRoom)
     socket.on('join-room', joinRoom)
     socket.on('start-sharing', startSharing)
     socket.on('stop-sharing', stopSharing)
+    socket.on('send-message', addMessage)
 }
